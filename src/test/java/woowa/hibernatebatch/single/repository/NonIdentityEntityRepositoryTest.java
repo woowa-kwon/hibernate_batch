9e3d3f12dc37eea52db19897061e5a41928b90c3
권순규 <sgkwon@woowahan.com>
2020-09-20 23:34:51 +0900

    예제 수정

--- src/test/java/woowa/hibernatebatch/single/repository/NonIdentityEntityRepositoryTest.java
+++ src/test/java/woowa/hibernatebatch/single/repository/NonIdentityEntityRepositoryTest.java
@@ -8,10 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.util.StopWatch;
 import woowa.hibernatebatch.TestSupport;
 import woowa.hibernatebatch.single.model.NonIdentityEntity;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
 import java.util.stream.LongStream;
 
@@ -56,9 +58,8 @@ class NonIdentityEntityRepositoryTest extends TestSupport {
     @DisplayName("엔티티 한건씩 save 호출")
     @Test
     void saveAll() throws Exception {
-        final List<NonIdentityEntity> nonIdentityEntities = LongStream.rangeClosed(1, 200)
-                .mapToObj(NonIdentityEntity::of)
-                .collect(Collectors.toList());
+        final int size = 5;
+        final List<NonIdentityEntity> nonIdentityEntities = createNonIdentityEntities(size);
 
         for (NonIdentityEntity nonIdentityEntity : nonIdentityEntities) {
             nonIdentityEntityRepository.save(nonIdentityEntity);
@@ -69,12 +70,48 @@ class NonIdentityEntityRepositoryTest extends TestSupport {
     @DisplayName("엔티티 목록 전체를 saveAll 호출")
     @Test
     void saveAll2() throws Exception {
-        final List<NonIdentityEntity> nonIdentityEntities = LongStream.rangeClosed(1, 200)
-                .mapToObj(NonIdentityEntity::of)
-                .collect(Collectors.toList());
+        final int size = 5;
+        final List<NonIdentityEntity> nonIdentityEntities = createNonIdentityEntities(size);
 
         nonIdentityEntityRepository.saveAll(nonIdentityEntities);
         flush();
+    }
+
+    @DisplayName("500건씩 분할하여 총 10,000건 저장")
+    @Test
+    void saveLoop() throws Exception {
+        final AtomicLong atomicLong = new AtomicLong();
+        final int totalSize = 10_000;
+        final int chunkSize = 500;
+        final int loopSize = totalSize / chunkSize;
+
+        final List<NonIdentityEntity> nonIdentityEntities = createNonIdentityEntities(totalSize);
+
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        for (int i = 0; i < loopSize; i++) {
+            final int fromIndex = i * chunkSize;
+            final int toIndex = fromIndex + chunkSize;
+            final List<NonIdentityEntity> subList = nonIdentityEntities.subList(fromIndex, toIndex);
+            nonIdentityEntityRepository.saveAll(subList);
+            flush();
+        }
+        stopWatch.stop();
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println("결과(초) : " + stopWatch.getTotalTimeSeconds());
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+
+    private List<NonIdentityEntity> createNonIdentityEntities(int size) {
+        final List<NonIdentityEntity> nonIdentityEntities = LongStream.rangeClosed(1, size)
+                .mapToObj(NonIdentityEntity::of)
+                .collect(Collectors.toList());
+        return nonIdentityEntities;
     }
 
     @DisplayName("저장한 목록 전체 한번에 업데이트")
@@ -82,19 +119,42 @@ class NonIdentityEntityRepositoryTest extends TestSupport {
     void updateAll() throws Exception {
         final int size = 10;
         insertTestValues(INSERT_NON_IDENTITY, nonIdentityParameters(size));
+        final List<NonIdentityEntity> nonIdentityEntities = nonIdentityEntityRepository.findAll();
+        nonIdentityEntities.forEach(NonIdentityEntity::plus);
 
-        Pageable pageable = PageRequest.of(0, size);
+        flush();
+    }
+
+    @DisplayName("10,000 건 저장후 500 건씩 분할 조회하여 업데이트")
+    @Test
+    void updateLoop() throws Exception {
+        final int insertSize = 10_000;
+        insertTestValues(INSERT_NON_IDENTITY, nonIdentityParameters(insertSize));
+
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        final int pageSize = 500;
+
+        Pageable pageable = PageRequest.of(0, pageSize);
         while (true) {
             final Slice<NonIdentityEntity> slice = nonIdentityEntityRepository.findAllIdentityEntities(pageable);
-            final List<NonIdentityEntity> identityEntities = slice.getContent();
-            identityEntities.forEach(NonIdentityEntity::plus);
+            final List<NonIdentityEntity> nonIdentityEntities = slice.getContent();
+            nonIdentityEntities.forEach(NonIdentityEntity::plus);
+            flush();
             if (slice.isLast()) {
                 break;
             }
             pageable = slice.nextPageable();
         }
+        stopWatch.stop();
 
-        flush();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println("결과(초) : " + stopWatch.getTotalTimeSeconds());
+        System.out.println();
+        System.out.println();
+        System.out.println();
     }
 
 }
